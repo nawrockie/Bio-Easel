603d295314f89c6471c92df7d33370d006fe9560
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2024-11-15 19:13:07 -0500

    Adjacent insertions and deletions now printed on 1 lien

--- scripts/esl-alicompare2rf.pl
+++ scripts/esl-alicompare2rf.pl
@@ -94,7 +94,9 @@ for($i = 0; $i < $nseq; $i++) {
   my @asqstring_A = split("", $asqstring);
   my $rfpos = 0;
   my $sqpos = 0;
-
+  my @out_str_A = ();
+  my @out_desc_A = ();
+  
   # determine first and final apos with a nongap residue
   my $apos = 0;
   my $spos = $alen+1;
@@ -115,12 +117,12 @@ for($i = 0; $i < $nseq; $i++) {
   for($apos = 1; $apos <= $alen; $apos++) { 
     my $sqchar = $asqstring_A[($apos-1)];
     my $rfchar = $rf_A[($apos-1)];
-    my $orig_sqchar = $sqchar;
-    my $orig_rfchar = $rfchar;
-    $sqchar =~ tr/a-z/A-Z/; # uppercase-ize
-    $rfchar =~ tr/a-z/A-Z/; # uppercase-ize
-    my $sq_is_gap = ($sqchar =~ m/[A-Z]/) ? 0 : 1;
-    my $rf_is_gap = ($rfchar =~ m/[A-Z]/) ? 0 : 1;
+    my $tmp_sqchar = $sqchar;
+    my $tmp_rfchar = $rfchar;
+    $tmp_sqchar =~ tr/a-z/A-Z/; # uppercase-ize
+    $tmp_rfchar =~ tr/a-z/A-Z/; # uppercase-ize
+    my $sq_is_gap = ($tmp_sqchar =~ m/[A-Z]/) ? 0 : 1;
+    my $rf_is_gap = ($tmp_rfchar =~ m/[A-Z]/) ? 0 : 1;
     my $desc = undef;
     if(! $rf_is_gap) { $rfpos++; }
     if(! $sq_is_gap) { $sqpos++; }
@@ -136,13 +138,165 @@ for($i = 0; $i < $nseq; $i++) {
           $desc = "deletion";
         }
       }
-      elsif($rfchar ne $sqchar) { 
+      elsif($tmp_rfchar ne $tmp_sqchar) { 
         $desc = "substitution";
       }
     }
     if(defined $desc) { 
-      printf("%-30s  %5d  %5d  %5d  %6s  %6s  $desc\n", 
-             $seq_name, $rfpos, $sqpos, $apos, $orig_rfchar, $orig_sqchar);
+      push(@out_str_A, sprintf("%s\t%d\t%d\t%d\t%s\t%s", 
+                               $seq_name, $rfpos, $sqpos, $apos, $rfchar, $sqchar));
+      push(@out_desc_A, $desc);
     }
   }
+
+  # output for all RF positions, we can go to alen (rflen must be <= alen)
+  my $nlines = scalar(@out_str_A);
+  my $prv_desc = undef;
+  my ($cur_seq_name, $cur_rfpos,  $cur_sqpos, $cur_apos, $cur_rfchar, $cur_sqchar, $cur_desc);
+  my $cur_sqpos_start = undef;
+  my $cur_sqpos_end   = undef;
+  my $cur_apos_start  = undef;
+  my $cur_apos_end    = undef;
+  my $cur_rfpos_start = undef;
+  my $cur_rfpos_end   = undef;
+  my $cur_sqchar_str  = "";
+  my $cur_rfchar_str  = "";
+  
+  for(my $l = 0; $l < $nlines; $l++) { 
+    my $cur_str = $out_str_A[$l];
+    $cur_desc = $out_desc_A[$l];
+
+    # if substitution - output it
+    # if deletion, keep going until sqpos changes, then output summary line
+    # if insertion, keep going until rfpos changes, then output summary line
+    if($cur_desc eq "substitution") {
+      # output any insertion or deletion strings we have
+      if(defined $cur_sqpos_start) {
+        output_summary_insertion_string($cur_seq_name, $cur_rfpos, $cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end, $cur_sqchar_str);
+        ($cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+        $cur_sqchar_str = "";
+      }
+      if(defined $cur_rfpos_start) {
+        output_summary_deletion_string($cur_seq_name, $cur_rfpos_start, $cur_rfpos_end, $cur_sqpos, $cur_apos_start, $cur_apos_end, $cur_rfchar_str);
+        ($cur_rfpos_start, $cur_rfpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+        $cur_rfchar_str = "";
+      }
+      print $cur_str . "\tsubstitution\n";
+    }
+    elsif($cur_desc eq "deletion") {
+      # output any insertion strings we have
+      if(defined $cur_sqpos_start) {
+        output_summary_insertion_string($cur_seq_name, $cur_rfpos, $cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end, $cur_sqchar_str);
+        ($cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+        $cur_sqchar_str = "";
+      }
+
+      my @el_A = split(/\t/, $cur_str);
+      if(scalar(@el_A) != 6) { die "ERROR internal coding problem"; }
+      ($cur_seq_name, $cur_rfpos,  $cur_sqpos, $cur_apos, $cur_rfchar, $cur_sqchar) = @el_A;
+
+      # build up the deletion line
+      if(! defined $cur_rfpos_start) {
+        $cur_rfpos_start = $cur_rfpos;
+        $cur_apos_start  = $cur_apos;
+        $cur_rfchar_str  = $cur_rfchar;
+      }
+      else {
+        $cur_rfchar_str .= $cur_rfchar;
+      }
+      $cur_rfpos_end = $cur_rfpos;
+      $cur_apos_end  = $cur_apos;
+      # sqpos doesn't change
+    }
+    elsif($cur_desc eq "insert-after-RF-position") {
+      # output any deletion strings we have
+      if(defined $cur_rfpos_start) {
+        output_summary_deletion_string($cur_seq_name, $cur_rfpos_start, $cur_rfpos_end, $cur_sqpos, $cur_apos_start, $cur_apos_end, $cur_rfchar_str);
+        ($cur_rfpos_start, $cur_rfpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+        $cur_rfchar_str = "";
+      }
+
+      my @el_A = split(/\t/, $cur_str);
+      if(scalar(@el_A) != 6) { die "ERROR internal coding problem"; }
+      ($cur_seq_name, $cur_rfpos,  $cur_sqpos, $cur_apos, $cur_rfchar, $cur_sqchar) = @el_A;
+
+      # build up the insertion line
+      if(! defined $cur_sqpos_start) {
+        $cur_sqpos_start = $cur_sqpos;
+        $cur_apos_start  = $cur_apos;
+        $cur_sqchar_str  = $cur_sqchar;
+      }
+      else {
+        $cur_sqchar_str .= $cur_sqchar;
+      }
+      $cur_sqpos_end = $cur_sqpos;
+      $cur_apos_end  = $cur_apos;
+    }
+  }
+  if(defined $cur_sqpos_start) {
+    output_summary_insertion_string($cur_seq_name, $cur_rfpos, $cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end, $cur_sqchar_str);
+    ($cur_sqpos_start, $cur_sqpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+    $cur_sqchar_str = "";
+  }
+  if(defined $cur_rfpos_start) {
+    output_summary_deletion_string($cur_seq_name, $cur_rfpos_start, $cur_rfpos_end, $cur_sqpos, $cur_apos_start, $cur_apos_end, $cur_rfchar_str);
+    ($cur_rfpos_start, $cur_rfpos_end, $cur_apos_start, $cur_apos_end) = (undef, undef, undef, undef);
+    $cur_rfchar_str = "";
+  }
+}
+
+#################################################################
+# Subroutine: output_summary_insertion_string()
+#
+# Arguments:
+#   $seq_name:    sequence name
+#   $rfpos:       reference position
+#   $sqpos_start: start sequence position 
+#   $sqpos_end:   end sequence position 
+#   $apos_start:  start alignment position
+#   $apos_end:    end alignment position
+#   $sqchar_str:  sequence string
+#
+#################################################################
+sub output_summary_insertion_string { 
+  my $sub_name = "output_summary_insertion_string";
+  my $nargs_expected = 7;
+  if(scalar(@_) != $nargs_expected) { die "ERROR $sub_name entered with wrong number of input args" }
+
+  my ($seq_name, $rfpos, $sqpos_start, $sqpos_end,  $apos_start, $apos_end, $sqchar_str) = @_;
+
+  my $sqpos_str = ($sqpos_start eq $sqpos_end) ? $sqpos_start : $sqpos_start . ".." . $sqpos_end;
+  my $apos_str  = ($apos_start  eq $apos_end)  ? $apos_start  : $apos_start  . ".." . $apos_end;
+  printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", 
+         $seq_name, $rfpos, $sqpos_str, $apos_str, "-", $sqchar_str, "insert-after-RF-position");
+
+  return;
+}
+
+#################################################################
+# Subroutine: output_summary_deletion_string()
+#
+# Arguments:
+#   $seq_name:    sequence name
+#   $rfpos_start: start reference position
+#   $rfpos_end:   end reference position
+#   $sqpos:       sequence position 
+#   $apos_start:  start alignment position
+#   $apos_end:    end alignment position
+#   $rfchar_str:  reference string
+#
+#################################################################
+sub output_summary_deletion_string { 
+  my $sub_name = "output_summary_deletion_string";
+  my $nargs_expected = 7;
+  if(scalar(@_) != $nargs_expected) { die "ERROR $sub_name entered with wrong number of input args" }
+
+  my ($seq_name, $rfpos_start, $rfpos_end, $sqpos, $apos_start, $apos_end, $rfchar_str) = @_;
+
+  my $rfpos_str = ($rfpos_start eq $rfpos_end) ? $rfpos_start : $rfpos_start . ".." . $rfpos_end;
+  my $apos_str  = ($apos_start  eq $apos_end)  ? $apos_start  : $apos_start  . ".." . $apos_end;
+  printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", 
+         $seq_name, $rfpos_str, $sqpos, $apos_str, $rfchar_str, "-", "deletion");
+
+  return;
 }
